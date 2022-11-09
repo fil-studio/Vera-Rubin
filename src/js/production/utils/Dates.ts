3c3f0f42542722f63f1ffb4a113603eced9e6ff4
Martí Fenosa <marti.fenosa@gmail.com>
2022-11-09 15:10:04 +0100

    Date with month

--- src/js/production/utils/Dates.ts
+++ src/js/production/utils/Dates.ts
@@ -3,11 +3,21 @@ const checkLength = (num:number) : string => {
 	return number;
 }
 
+const getMonth = (num:number) : string => {
+
+	const isES = document.documentElement.getAttribute('lang') === 'es-ES';
+
+	const months = isES ? ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'] : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	
+	return months[num];
+}
+
 export const getFormatDate = (date:Date) => {
 
 	const d = {	
 		y: checkLength(date.getFullYear()),
 		m: checkLength(date.getMonth() + 1),
+		month: getMonth(date.getMonth()),
 		d: checkLength(date.getDate()),
 		h: checkLength(date.getHours()),
 		min: checkLength(date.getMinutes()),
@@ -23,7 +33,7 @@ export const formatDate = (date:Date) => {
 
 	const isES = document.documentElement.getAttribute('lang') === 'es-ES';	
 
-	const formattedDate = isES ? `${d.d}/${d.m}/${d.y} - ${d.h}:${d.min}:${d.s}` : `${d.m}/${d.d}/${d.y} - ${d.h}:${d.min}:${d.s}`;
+	const formattedDate = isES ? `${d.d} de ${d.month}, ${d.y} - ${d.h}:${d.min}:${d.s}` : `${d.month} ${d.d}, ${d.y} - ${d.h}:${d.min}:${d.s}`;
 
 	return formattedDate;
 }
@@ -34,7 +44,7 @@ export const formatDateString = (date:Date) => {
 
 	const isES = document.documentElement.getAttribute('lang') === 'es-ES';	
 
-	const formattedDate = isES ? `${d.d}.${d.m}.${d.y}` : `${d.m}.${d.d}.${d.y}`;
+	const formattedDate = isES ? `${d.d} de ${d.month}, ${d.y}` : `${d.month} ${d.d}, ${d.y}`;
 
 	return formattedDate;
 }
